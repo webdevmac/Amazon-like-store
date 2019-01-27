42d6bde87bc35b76c42677c922c361c384d9bbb5
Web-dev <webdevmac@users.noreply.github.com>
2019-01-27 08:48:14 -0500

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -64,15 +64,15 @@ function start() {
                         ],
                         function (error) {
                             if (error) throw err;
-                            console.log("Item purchase successfully!");
-
+                            console.log('\n' + "Your purchase for the " + chosenItem.product_name + " was successfull! Your cost is $" + chosenItem.price + '\n');
+                            connection.end();
                         }
                     );
                 }
                 else {
                     // Inventory not enough, so enter another quantity
-                    console.log("Not enough items in stock. Try again...");
-
+                    console.log('\n'+ "Not enough items in stock! Try again..." +'\n');
+                    start();
                 }
             });
 
@@ -80,29 +80,31 @@ function start() {
 
 
 }
-
-// connection.connect(function(err) {
-//     if (err) throw err;
-//     console.log("Connected!");
-//     var sql = "INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES ?";
-//     var values = [
-//       ['D191', 'Dog Collar', 'Pets', '9.00', '10'],
-//       ['D192', 'Dog Leash', 'Pets', '12.00', '15'],
-//       ['F193', 'Chair', 'Furniture', '150.00', '4'],
-//       ['F194', 'Table', 'Furniture', '1400.00', '1'],
-//       ['K195', 'Set of Glassses', 'Houseware', '11.00', '17'],
-//       ['K196', 'Towels', 'Houseware', '19.00', '4'],
-//       ['E197', 'TV', 'Electronics', '129.00', '6'],
-//       ['E198', 'Computer', 'Electronics', '365.00', '7'],
-//       ['A199', 'Ice Scraper', 'Auto', '9.99', '9'],
-//       ['A200', 'Windshield Wipers', 'Auto', '19.50', '12'],
-//       ['A201', 'Oil Filter', 'Auto', '8.50', '16'],
-//       ['A202', 'Car Matts', 'Auto', '30.00', '10'],
-//       ['C203', 'Mens T-Shirts', 'Clothing', '9.50', '7'],
-//       ['C204', 'Mens Socks', 'Clothing', '6.50', '6']
-//     ];
-//     connection.query(sql, [values], function (err, result) {
-//       if (err) throw err;
-//       console.log("Number of records inserted: " + result.affectedRows);
-//     });
-//   });
+// Run this funtion to enter new Items into the Database......
+function enterProductsToDatabase() {
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) VALUES ?";
+    var values = [
+      ['D191', 'Dog Collar', 'Pets', '9.00', '10'],
+      ['D192', 'Dog Leash', 'Pets', '12.00', '15'],
+      ['F193', 'Chair', 'Furniture', '150.00', '4'],
+      ['F194', 'Table', 'Furniture', '1400.00', '1'],
+      ['K195', 'Set of Glassses', 'Houseware', '11.00', '17'],
+      ['K196', 'Towels', 'Houseware', '19.00', '4'],
+      ['E197', 'TV', 'Electronics', '129.00', '6'],
+      ['E198', 'Computer', 'Electronics', '365.00', '7'],
+      ['A199', 'Ice Scraper', 'Auto', '9.99', '9'],
+      ['A200', 'Windshield Wipers', 'Auto', '19.50', '12'],
+      ['A201', 'Oil Filter', 'Auto', '8.50', '16'],
+      ['A202', 'Car Matts', 'Auto', '30.00', '10'],
+      ['C203', 'Mens T-Shirts', 'Clothing', '9.50', '7'],
+      ['C204', 'Mens Socks', 'Clothing', '6.50', '6']
+    ];
+    connection.query(sql, [values], function (err, result) {
+      if (err) throw err;
+      console.log("Number of records inserted: " + result.affectedRows);
+    });
+  });
+}
